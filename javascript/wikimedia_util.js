df3629d49afa08f299c31726a03b6957bcdec5b5
Dare <ogundipe.obadare@gmail.com>
2018-01-23 16:35:49 -0500

    Working on wikimedia util

---
+++ javascript/wikimedia_util.js
@@ -0,0 +1,8 @@
+var wiki_api = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&titles=Stack%20Overflow";
+d3.json(wiki_api, function(error, wiki) {
+  console.log(wiki);
+});
+
+
+// gets stack overflows intro Page
+// https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=Stack%20Overflow
